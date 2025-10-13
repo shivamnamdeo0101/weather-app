e078045eaa97fcec9e36a357e78c9f62fe772ddd
shivamnamdeo0101 <shivamnamdeo0101@gmail.com>
2025-10-14 00:06:28 +0530

    origin updated

--- server/weather-cache/src/main/java/com/shivam/weather_cache/config/WebConfig.java
+++ server/weather-cache/src/main/java/com/shivam/weather_cache/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://57.152.95.75:3001")
+                        .allowedOrigins("http://57.152.95.75:3001","http://localhost:3001")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
